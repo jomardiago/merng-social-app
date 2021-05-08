2865e9aabb0d7948cc25c5aeaf5ebec2d0c04ff3
Jose-Diago <jose.diago@ibm.com>
2021-05-08 09:13:20 +0800

    Create getPosts query.

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ import gql from 'graphql-tag';
 import mongoose from 'mongoose';
 import config from 'config';
 
+import Post from './models/Post.js';
+
 const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query {
-        sayHi: String!
+        getPosts: [Post]
     }
 `;
 
 const resolvers = {
     Query: {
-        sayHi() {
-            return 'Hello World!';
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (error) {
+                throw new Error(error);
+            }
         }
     }
 };
