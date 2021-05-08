fc84855612091a667a3ca0bf1a8398e1c21721fc
Jose-Diago <jose.diago@ibm.com>
2021-05-08 09:19:36 +0800

    Create a graphql folder to house the resolvers and typeDefs.

---
+++ graphql/resolvers.js
@@ -0,0 +1,14 @@
+import Post from '../models/Post.js';
+
+export default {
+    Query: {
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    }
+};
--- server.js
+++ server.js
@@ -1,34 +1,9 @@
 import { ApolloServer } from 'apollo-server';
-import gql from 'graphql-tag';
 import mongoose from 'mongoose';
 import config from 'config';
 
-import Post from './models/Post.js';
-
-const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`;
-
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (error) {
-                throw new Error(error);
-            }
-        }
-    }
-};
+import typeDefs from './graphql/typeDefs.js';
+import resolvers from './graphql/resolvers.js';
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
