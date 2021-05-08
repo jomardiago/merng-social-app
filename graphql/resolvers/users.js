5d7e72b50a877197181ebc2167330f945be0ded1
Jose-Diago <jose.diago@ibm.com>
2021-05-08 14:59:34 +0800

    Using validator util and removing try catch to not override UserInputError.

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,26 +4,25 @@ import config from 'config';
 import { UserInputError } from 'apollo-server';
 
 import User from '../../models/User.js';
+import { validateRegisterInput } from '../../utils/validators.js';
 
 export default {
     Mutation: {
-        async register(_, { registerInput: {username, email, password, confirmPassword} }) {
-            try {
-                const user = await User.findOne({ username });
+        async register(_, args) {
+            let { username, email, password, confirmPassword } = args.registerInput;
+            const { errors, valid } = validateRegisterInput(args.registerInput);
+            const user = await User.findOne({ username });
 
-                if (user) {
-                    throw new UserInputError('Username is taken', { errors: {username: 'This username is taken'} });
-                } else if (password !== confirmPassword) {
-                    throw new UserInputError('Passwords do not match', { errors: {confirmPassword: 'Passwords do not match'} });
-                } else {
-                    password = await bcrypt.hash(password, 12);
-                    const newUser = new User({ email, username, password, createdAt: new Date().toISOString() });
-                    const result = await newUser.save();
-                    const token = jwt.sign({ id: result.id, email, username }, config.secretKey, { expiresIn: '1h' });
-                    return { ...result._doc, id: result._id, token };
-                }
-            } catch (error) {
-                throw new Error(error);
+            if (user) {
+                throw new UserInputError('Username is taken', { errors: {username: 'This username is taken'} });
+            } else if (!valid) {
+                throw new UserInputError('Errors', { errors });
+            } else {
+                password = await bcrypt.hash(password, 12);
+                const newUser = new User({ email, username, password, createdAt: new Date().toISOString() });
+                const result = await newUser.save();
+                const token = jwt.sign({ id: result.id, email, username }, config.secretKey, { expiresIn: '1h' });
+                return { ...result._doc, id: result._id, token };
             }
         }
     }
