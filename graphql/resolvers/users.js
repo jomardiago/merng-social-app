532cabfeecf9d75bf6901ec53568ac532cdee790
Jose-Diago <jose.diago@ibm.com>
2021-05-08 20:42:12 +0800

    Added login functionality.

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,10 +4,37 @@ import config from 'config';
 import { UserInputError } from 'apollo-server';
 
 import User from '../../models/User.js';
-import { validateRegisterInput } from '../../utils/validators.js';
+import { validateLoginInput, validateRegisterInput } from '../../utils/validators.js';
+
+function generateToken({ _id, email, username }) {
+    return jwt.sign({ _id, email, username }, config.secretKey, { expiresIn: '1h' });
+};
 
 export default {
     Mutation: {
+        async login(_, args) {
+            const { errors, valid } = validateLoginInput(args.loginInput);
+            const { username, password } = args.loginInput;
+
+            if (!valid) {
+                throw new UserInputError('Errors', { errors });
+            } else {
+                const user = await User.findOne({ username });
+
+                if (!user) {
+                    throw new UserInputError('User not found', { errors: {general: 'User does not exists'} });
+                } else {
+                    const match = await bcrypt.compare(password, user.password);
+                    const { id, email, username } = user;
+                    if (match) {
+                        const token = generateToken(user);
+                        return { ...user._doc, id: user._id, token };
+                    } else {
+                        throw new UserInputError('User not found', { errors: {username: 'User does not exists'} });
+                    }
+                }
+            }
+        },
         async register(_, args) {
             let { username, email, password, confirmPassword } = args.registerInput;
             const { errors, valid } = validateRegisterInput(args.registerInput);
@@ -21,7 +48,7 @@ export default {
                 password = await bcrypt.hash(password, 12);
                 const newUser = new User({ email, username, password, createdAt: new Date().toISOString() });
                 const result = await newUser.save();
-                const token = jwt.sign({ id: result.id, email, username }, config.secretKey, { expiresIn: '1h' });
+                const token = generateToken(result);
                 return { ...result._doc, id: result._id, token };
             }
         }
--- utils/validators.js
+++ utils/validators.js
@@ -22,3 +22,17 @@ export function validateRegisterInput({ username, email, password, confirmPasswo
 
     return { errors, valid: Object.keys(errors).length < 1 };
 }
+
+export function validateLoginInput({ username, password }) {
+    const errors = {};
+
+    if (username.trim() === '') {
+        errors.username = 'Username is required';
+    }
+
+    if (password === '') {
+        errors.password = 'Password is required'
+    }
+
+    return { errors, valid: Object.keys(errors).length < 1 };
+}
