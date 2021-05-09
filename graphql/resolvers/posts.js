8588a4a401220f1de9f2577ffccb8a77037c2a27
Jose-Diago <jose.diago@ibm.com>
2021-05-09 12:18:47 +0800

    Added sorting of getPosts by createdAt

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -7,7 +7,7 @@ export default {
     Query: {
         async getPosts() {
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({ createdAt: -1 });
                 return posts;
             } catch (error) {
                 throw new Error(error);
