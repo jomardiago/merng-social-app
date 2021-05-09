17c9afe4a36d96b4627322e491540f596c9f2ed0
Jose-Diago <jose.diago@ibm.com>
2021-05-09 13:22:41 +0800

    Adding deleteComment mutation.

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-import { UserInputError } from 'apollo-server';
+import { UserInputError, AuthenticationError } from 'apollo-server';
 
 import validateAuth from '../../utils/validateAuth.js';
 import Post from '../../models/Post.js';
@@ -24,6 +24,23 @@ export default {
                     return post;
                 } catch (error) {
                     throw new Error(error);
+                }
+            } else {
+                throw new Error('Post not found');
+            }
+        },
+        async deleteComment(_, { postId, commentId }, context) {
+            const user = validateAuth(context);
+            const post = await Post.findById(postId);
+
+            if (post) {
+                if (user.username !== post.comments.find(comment => comment.id === commentId).username) {
+                    throw new AuthenticationError('You can only delete your own comment');
+                } else {
+                    const updatedComments = post.comments.filter(comment => comment.id !== commentId);
+                    post.comments = updatedComments;
+                    post.save();
+                    return post;
                 }
             } else {
                 throw new Error('Post not found');
