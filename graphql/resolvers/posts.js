79d17911156b66e8d29d32a66762f1b075efb1b7
Jose-Diago <jose.diago@ibm.com>
2021-05-09 11:20:27 +0800

    Added createPost mutation.

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 import Post from '../../models/Post.js';
+import validateAuth from '../../utils/validateAuth.js';
 
 export default {
     Query: {
@@ -15,6 +16,27 @@ export default {
                 const post = await Post.findById(postId);
                 if (!post) throw new Error('Post not found');
                 return post;
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_, { body }, context) {
+            try {
+                const user = validateAuth(context);
+
+                if (user) {
+                    const newPost = Post({
+                        body,
+                        user: user.id,
+                        username: user.username,
+                        createdAt: new Date().toISOString()
+                    });
+
+                    const post = await newPost.save();
+                    return post;
+                }
             } catch (error) {
                 throw new Error(error);
             }
